cad4f8722835b350b13d87b18a1bebb7fca113b4
YangdongYe <yangdong.y@husky.neu.edu>
2015-02-08 02:27:59 -0500

    20150208as2

--- part-1.js
+++ part-1.js
@@ -5,28 +5,62 @@
 
 //Information about cities can be easily represented as JavaScript objects
 var city1 = {
-		name:"Boston",
-		pop:657000,
-		"year founded":1630
-	};
+	name:"Boston",
+	pop:657000,
+	"year founded":1630
+};
 var city2 = {
-		name:"Cambridge",
-		pop:120000,
-		"year founded":1631
-	};
-	
+	name:"Cambridge",
+	pop:120000,
+	"year founded":1631
+};
+
 //TODO: write a function that takes two city objects as parameter
 //and returns the name of city that has a larger population
 function whichCityIsLarger(a,b){
 	//hint: since a and b are objects, you can access their "pop" property using a.pop
 	//compare populations, and return the "name" property of the appropriate city
-
+	//console.log( a,b );
+	if (a.pop > b.pop) {
+		return "The larger city is "+a.name;
+	} else if (a.pop < b.pop) {
+		return "The larger city is "+b.name;
+	} else {
+		return "They are the same";
+	}
 }
-console.log( whichCityIsLarger(city1,city2) ); 
+
+console.log( whichCityIsLarger(city1,city2) );
 
 
 //TODO: write a function that returns the name of the city that is older
 function whichCityIsOlder(a,b){
 	//hint: more or less the same as above, but notice something weird about the property name "year founded"?
 	//Can you still use the dot notation? If not, what should you do?
+	var sa = JSON.stringify(a);
+	var sb = JSON.stringify(b);
+	if (YearFounded(sa) < YearFounded(sb)) {
+		return "The older city is "+a.name;
+	} else if (YearFounded(sa) > YearFounded(sb)) {
+		return "The older city is "+b.name;
+	} else {
+		return "They are the same";
+	}
 }
+
+var StringStart = 0;
+var StringEnd = 0;
+
+function YearFounded(s){
+	var CharStart = s.indexOf("year founded");
+	for (i = CharStart; i<s.length; i++){
+		if (s[i]==":") {
+			StringStart = i+1;
+		}
+		if (s[i]=="}") {
+			StringEnd = i;
+		};
+	};
+	return s.slice(StringStart,StringEnd);
+}
+console.log( whichCityIsOlder(city1,city2) );
--- part-2.js
+++ part-2.js
@@ -9,13 +9,30 @@ var newArray = [];
 //TODO: populate the new array with 1000 new random values between 50 and 200
 //hint: array.push
 //hint: Math.random() creates a random value between 0 and 1
+var i=0;
+//var sum = 0;
+while (i<1000) {
+    var num = Math.random();
+    if (num>=0.25) {
+        newArray.push(num*200);
+//        sum = sum+newArray[i];
+        i++;
+    }
+}
 
 
 //TODO: after you populate the array, find the average of all the values in the array
-
+//console.log(sum/1000);
+//var arraySum0 = 0;
+//for (i=0;i<1000;i++) {
+//    arraySum0 = arraySum0+newArray[i];
+//}
+//console.log(arraySum0/1000);
 var arraySum = 0;
 newArray.forEach(function(arrayElement){
 	//hint: here, we are "looping" through the array, accessing each array element one by one
 	//we can add the value to arraySum
 	//and finally, we divide arraySum by the total number of array elements
+    arraySum = arraySum + arrayElement;
 });
+console.log(arraySum/1000);
